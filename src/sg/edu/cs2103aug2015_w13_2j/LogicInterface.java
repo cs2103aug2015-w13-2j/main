f0f1fcad7b869444cff4a3717efff2291a79bc9d
SmokinClove <van.tuong.nguyen@hotmail.com>
2015-09-27 23:33:42 +0800

    Created LogicInterface

---
+++ src/sg/edu/cs2103aug2015_w13_2j/LogicInterface.java
@@ -0,0 +1,15 @@
+package sg.edu.cs2103aug2015_w13_2j;
+
+import java.util.*;
+/*This class contains methods for classes implementing the abstract class LogicInterface
+Haven't put in edit due to uncertainty of format 
+*/
+
+public interface LogicInterface {
+	
+    public void addTask(Task task);
+    public void deleteTask(Task task);
+    public void archiveTask(Task task);
+    public void sort();
+    public ArrayList<Task> getAllTask();
+}
