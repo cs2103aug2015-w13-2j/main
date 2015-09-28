7e98f2321b79eb82846d5aebb2e9cefbbfbe82c3
SmokinClove <van.tuong.nguyen@hotmail.com>
2015-09-29 00:17:37 +0800

    Added methods to Logic class that implements LogicInterface WIP for #30

--- src/sg/edu/cs2103aug2015_w13_2j/Logic.java
+++ src/sg/edu/cs2103aug2015_w13_2j/Logic.java
@@ -2,7 +2,7 @@ package sg.edu.cs2103aug2015_w13_2j;
 
 import java.util.*;
 
-public class Logic {
+public class Logic implements LogicInterface{
     private ArrayList<Task> tasks;
     
     public Logic(){
@@ -11,5 +11,23 @@ public class Logic {
     
     public void addTask(Task task){
     	tasks.add(task);
+    }
+    
+    public void deleteTask(Task task){
+    	
+    }
+    public void archiveTask(Task task){
+    	
+    }
+    public void sort(){
+    	
+    }
+    public ArrayList<Task> getAllTask(){
+    	return tasks;
+    }
+    
+    /*should display be in Logic ?*/
+    public void display(){
+    	
     }
 }
