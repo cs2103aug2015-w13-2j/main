7ee552239f6062df289fe084c8d44dcf4d1b19d1
SmokinClove <van.tuong.nguyen@hotmail.com>
2015-09-29 00:14:38 +0800

    Added addTask method to Logic WIP for #30

--- src/sg/edu/cs2103aug2015_w13_2j/Logic.java
+++ src/sg/edu/cs2103aug2015_w13_2j/Logic.java
@@ -1,6 +1,15 @@
 package sg.edu.cs2103aug2015_w13_2j;
 
+import java.util.*;
 
 public class Logic {
+    private ArrayList<Task> tasks;
     
+    public Logic(){
+        tasks = new ArrayList<Task>();	
+    }
+    
+    public void addTask(Task task){
+    	tasks.add(task);
+    }
 }
