79d58b345fd2ed2d254e305213c108b92c1e399b
NatashaKSS <xnagol@gmail.com>
2015-09-29 09:51:54 +0800

    Added test case for creating new Parser in ParserTest and constructor in Parser class
    WIP #34

--- src/sg/edu/cs2103aug2015_w13_2j/Parser.java
+++ src/sg/edu/cs2103aug2015_w13_2j/Parser.java
@@ -1,6 +1,25 @@
 package sg.edu.cs2103aug2015_w13_2j;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Arrays;
+
+/**
+ * Sample Add command format
+ * add -s <date/time> -e <date/time> -r <frequency/> -d <date/time> "Task name"
+ */
 public class Parser implements ParserInterface {
+	
+	private String commandLine;
+	
+	public Parser(String commandLine) {
+		this.commandLine = commandLine;
+	}
+	
+	public String getCommandLine () {
+		return this.commandLine;
+	}
+	
 	
 	
 }
--- src/sg/edu/cs2103aug2015_w13_2j/ParserTest.java
+++ src/sg/edu/cs2103aug2015_w13_2j/ParserTest.java
@@ -5,10 +5,16 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class ParserTest {
-
+	// Use the same Parser object across all test cases
+	private Parser parser;
+	
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void createParserTest() {
+		String sampleCommandLine = "add -s 23/09 -r 1week 'Task name'";
+		parser = new Parser(sampleCommandLine);
+		
+		assertEquals(sampleCommandLine, parser.getCommandLine());
 	}
 
 }
