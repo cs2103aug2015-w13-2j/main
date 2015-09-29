ed3581c73327d7a87fbb8667daae872c536bdca7
NatashaKSS <xnagol@gmail.com>
2015-09-29 10:52:21 +0800

    Added ParserState enum class. Passed ParserTest test case when traversing between states
    WIP #35
    
    Note:
    States represented in ParserState will help in determining how a certain
    command will be parsed.

--- src/sg/edu/cs2103aug2015_w13_2j/Parser.java
+++ src/sg/edu/cs2103aug2015_w13_2j/Parser.java
@@ -1,25 +1,61 @@
 package sg.edu.cs2103aug2015_w13_2j;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Arrays;
-
 /**
  * Sample Add command format
  * add -s <date/time> -e <date/time> -r <frequency/> -d <date/time> "Task name"
  */
 public class Parser implements ParserInterface {
 	
+	public enum ParserState {
+		PARSE_COMMAND_STATE {
+			public ParserState nextState(String commandLine) {
+				System.out.println("In PARSE_COMMAND_STATE");
+				return PARSE_TASK_NAME_STATE;
+			}
+			// Pass commandLine without command token (first token) to next state
+		}, 
+		
+		PARSE_TASK_NAME_STATE {
+			public ParserState nextState(String commandLine) {
+				System.out.println("In PARSE_TASK_NAME_STATE");
+				return PARSE_OPTIONS_STATE;
+			}
+			// Pass commandLine without task name token (last token) to next state
+		}, 
+		
+		PARSE_OPTIONS_STATE {
+			public ParserState nextState(String commandLine) {
+				System.out.println("In PARSE_OPTIONS_STATE");
+				return PARSE_END;
+			}
+			// Pass something to ending state, doesn't really matter what
+		},
+		
+		PARSE_END {
+			public ParserState nextState(String commandLine) {
+				System.out.println("In PARSE_END");
+				return null;
+			}
+		};
+		
+		public abstract ParserState nextState(String commandLine);
+		
+	}
+	
 	private String commandLine;
 	
+	/**
+	 * Parser constructor 
+	 * 
+	 * @param commandLine
+	 */
 	public Parser(String commandLine) {
 		this.commandLine = commandLine;
 	}
 	
-	public String getCommandLine () {
+	public String getCommandLine() {
 		return this.commandLine;
 	}
-	
 	
 	
 }
--- src/sg/edu/cs2103aug2015_w13_2j/ParserTest.java
+++ src/sg/edu/cs2103aug2015_w13_2j/ParserTest.java
@@ -3,18 +3,53 @@ package sg.edu.cs2103aug2015_w13_2j;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
 
 public class ParserTest {
 	// Use the same Parser object across all test cases
-	private Parser parser;
+	private Parser parser = null;
 	
+	@Before
+	public void setUp() {
+		String defaultCommandLine = "add -s 23/09 -r 1week 'Task name'";
+		parser = new Parser(defaultCommandLine);
+	}
+	
+	@After
+	public void cleanUp() {
+		parser = null;
+	}
 	
 	@Test
 	public void createParserTest() {
-		String sampleCommandLine = "add -s 23/09 -r 1week 'Task name'";
+		String sampleCommandLine = "add -s 23/09 -e 4pm *@(*#(!&@! 'Task name'";
 		parser = new Parser(sampleCommandLine);
 		
 		assertEquals(sampleCommandLine, parser.getCommandLine());
+	}
+	
+	@Test
+	public void parserStateTest() {
+		// Start from the PARSE_COMMAND_STATE
+		Parser.ParserState state = Parser.ParserState.PARSE_COMMAND_STATE;
+		String commandLine = parser.getCommandLine();
+		
+		// From PARSE_COMMAND_STATE to PARSE_TASK_NAME_STATE
+		state = state.nextState(commandLine);
+		assertEquals(Parser.ParserState.PARSE_TASK_NAME_STATE, state);
+		
+		// From PARSE_TASK_NAME_STATE to PARSE_OPTIONS_STATE
+		state = state.nextState(commandLine);
+		assertEquals(Parser.ParserState.PARSE_OPTIONS_STATE, state);
+		
+		// From PARSE_OPTIONS_STATE to PARSE_END
+		state = state.nextState(commandLine);
+		assertEquals(Parser.ParserState.PARSE_END, state);
+		
+		// Ended state traversal
+		state = state.nextState(commandLine);
+		assertEquals(null, state);
 	}
 
 }
