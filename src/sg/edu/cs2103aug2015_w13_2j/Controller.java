742929781b34227b41d9caf37dcaf2d5ea27ef08
NatashaKSS <xnagol@gmail.com>
2015-10-04 01:22:51 +0800

    Edited constructor and execution methods.
    Method stubs with description created.
    
    WIP #40

--- src/sg/edu/cs2103aug2015_w13_2j/Controller.java
+++ src/sg/edu/cs2103aug2015_w13_2j/Controller.java
@@ -1,5 +1,11 @@
 package sg.edu.cs2103aug2015_w13_2j;
 
+import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javafx.util.Pair;
+
 /**
  * Class that determines 
  * 
@@ -11,43 +17,25 @@ public class Controller {
 	 * Enum class representing all available commands supported
 	 */
 	public enum Commands {
-		add, delete, edit, mark, archive, retrieve, filter, summarise, 
-		export, help, exit
+		ADD, DELETE, EDIT, MARK, ARCHIVE, RETRIEVE, FILTER, SUMMARISE,
+		EXPORT, HELP, EXIT
 	}
 	
+	private static final Logger log = 
+			Logger.getLogger(Controller.class.getName());
+	private Task task;
+	private Vector<Pair<Parser.Token, String>> listOfTokens;
+	
 	/**
-	 * 
+	 * Constructor for CustomController class. 
+	 * Takes in a Parser object and initializes a listOfTokens.
 	 */
-	public enum ControllerState {
-		PARSE_COMMAND_STATE {
-			public ControllerState nextState(String commandLine) {
-				return PARSE_TASK_NAME_STATE;
-			}
-			// Pass commandLine without command token (first token) to next state
-		}, 
+	public Controller(Parser parser) {
+		this.listOfTokens = parser.getListOfTokens();
+		TaskAssembler taskAssembler = new TaskAssembler(this.listOfTokens);
+		task = taskAssembler.getAssembledTask();
 		
-		PARSE_TASK_NAME_STATE {
-			public ControllerState nextState(String commandLine) {
-				return PARSE_OPTIONS_STATE;
-			}
-			// Pass commandLine without task name token (last token) to next state
-		}, 
-		
-		PARSE_OPTIONS_STATE {
-			public ControllerState nextState(String commandLine) {
-				return PARSE_END;
-			}
-			// Pass something to ending state, doesn't really matter what
-		},
-		
-		PARSE_END {
-			public ControllerState nextState(String commandLine) {
-				return null;
-			}
-		};
-		
-		public abstract ControllerState nextState(String commandLine);
-		
+		startCommandExecution();
 	}
 	
 	/**
@@ -58,4 +46,36 @@ public class Controller {
 	public static Commands[] getCommands() {
 		return Commands.values();
 	}
+	
+	public Task getControllerTask() {
+		return this.task;
+	}
+	
+	/**
+	 * Executes command as specified by the user input
+	 * 
+	 * @throws   Error
+	 * 				Error when command is not of any accepted type
+	 * 
+	 */
+	private void startCommandExecution() {
+		
+	} 
+	
+	/**
+	 * Gets the command of a user input if it is a valid, reserved keyword.
+	 * This method only checks the first token of the list of tokens for a 
+	 * command, any subsequent commands (even if in a valid format), will be
+	 * ignored.
+	 * 
+	 * @return   A valid command of user input command line
+	 * 
+	 * @throws Error
+	 * 			Error when a token not of the RESERVED Token type is found 
+	 * 			to represent the command of the user's input
+	 */
+	private Commands getCommand() throws Error {
+		return null;
+	}
+	
 }
