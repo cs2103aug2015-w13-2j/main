c5dbe34ede076a05c3fac8b77da4203dc72345a0
SmokinClove <van.tuong.nguyen@hotmail.com>
2015-10-05 00:07:26 +0800

    Edited addTask() to add into one of the 3 arrayLists according to type

--- src/sg/edu/cs2103aug2015_w13_2j/Logic.java
+++ src/sg/edu/cs2103aug2015_w13_2j/Logic.java
@@ -14,21 +14,38 @@ public class Logic implements LogicInterface{
     private ArrayList<Task> tasks;
     private ArrayList<Task> events;//TBC if needed
     private ArrayList<Task> deadlines;//TBC if needed
-    private ArrayList<Task> floating;//TBC if needed
+    private ArrayList<Task> floats;//TBC if needed
     private ArrayList<Task> userView;//to store the user's latest requested list if any
                                      //eg. user filter(completed) all completed tasks will be store
                                      //here in case user wants to perform further request 
-    
+   /**
+    * Constructor for the Logic component
+    * 
+    */
     public Logic(){
         tasks = new ArrayList<Task>();	
         events = new ArrayList<Task>();	
         deadlines = new ArrayList<Task>();	
-        floating = new ArrayList<Task>();	
+        floats = new ArrayList<Task>();	
         userView = new ArrayList<Task>();
     }
     
+    /**
+     * Add a new task
+     * @param task
+     *            the new task to be added
+     * 
+     */
     public void addTask(Task task){
     	tasks.add(task);
+    	
+    	if(task.getType().equals("EVENT")){
+    		events.add(task);
+    	} else if (task.getType().equals("DUE")){
+    		deadlines.add(task);
+    	} else {
+    		floats.add(task);
+    	}
     }
     
     public Task getTask(int index){
