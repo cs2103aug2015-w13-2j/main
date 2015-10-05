998d1fa3ba36e4b43d4a02c6874a1ebc4336fec1
NatashaKSS <xnagol@gmail.com>
2015-10-05 20:39:21 +0800

    Refactored ControllerTest class
    WIP #60

--- src/sg/edu/cs2103aug2015_w13_2j/ControllerTest.java
+++ src/sg/edu/cs2103aug2015_w13_2j/ControllerTest.java
@@ -16,61 +16,107 @@ import javafx.util.Pair;
  */
 public class ControllerTest {
 
+	private Parser parser;
 	private Controller controller;
 	
+	/**
+	 * Sets up the test environment for the Controller class methods
+	 * 
+	 * @param commandLine
+	 * 			The command line that simulates the input from user
+	 */
 	private void setUpTestController(String commandLine) {
-		Parser parser = new Parser();
-		parser.parseCommand(commandLine);
-		System.out.println("Parsed TOKENS: " + parser.getListOfTokens());
+		this.parser = new Parser();
+		this.parser.parseCommand(commandLine);
 		controller = new Controller(parser);
 	}
 	
+	/**
+	 * Gets a list of tokens from the parser given a command line
+	 * 
+	 * Pre-condition:
+	 * Test environment for the Controller class has been set up. 
+	 * i.e. Parser object has been instantiated
+	 * 
+	 * @param commandLine
+	 * 			The command line that simulates the input from user
+	 * 
+	 * @return list of tokens representing the commandLine
+	 */
 	private Vector<Pair<Parser.Token, String>> generateListOfTokens(
 			String commandLine) {
-		Parser parser = new Parser();
-		parser.parseCommand(commandLine);
-		
-		return parser.getListOfTokens();
+
+		return this.parser.getListOfTokens();
 	}
 	
+	/**
+	 * Tests the getCommand method
+	 * Contains a mixture of valid and invalid commands tested
+	 */
 	@Test
 	public void getCommandTest() {
 		// Test for a valid command
 		String validCommand = "ADD 'Something'";
-		Vector<Pair<Parser.Token, String>> validCommandTokens = 
-				generateListOfTokens(validCommand);
 		Controller.Commands expectedValidCommand = Controller.Commands.ADD;
-		
-		setUpTestController(validCommand);
-		assertEquals(expectedValidCommand, controller.getCommand(validCommandTokens));
-	
-		// Test for a command that is not in accepted list of commands
+		getCommandValidTest(validCommand, expectedValidCommand);
+
+		// Test for an invalid command
 		String invalidCommand = "ADZZDSS";
 		String expectedErrorInvalidCommand = "Invalid command entered";
-		
-		try {
-			setUpTestController(invalidCommand);
-			controller.getCommand();
-			fail("getCommandTest - invalidCommand exception not thrown");
-		} catch (Error error) {
-			assertEquals(expectedErrorInvalidCommand, error.getMessage());
-		}
+		getCommandExceptionTest(invalidCommand, expectedErrorInvalidCommand);
 		
 		// Test for empty string command
 		String invalidCommandEmptyString = "";
 		String expectedErrorEmptyString = "Invalid command entered. "
 				+ "No user input detected.";
-		
-		try {
-			setUpTestController(invalidCommandEmptyString);
-			controller.getCommand();
-			fail("getCommandTest - invalidCommandEmptyString exception not thrown");
-		} catch (Error error) {
-			assertEquals(expectedErrorEmptyString, error.getMessage());
-		}
-		
+		getCommandExceptionTest(invalidCommandEmptyString, expectedErrorEmptyString);
 	}
 	
+	/**
+	 * Test for valid commands returned by getCommand method
+	 * 
+	 * @param validCommand 
+	 * 			The valid command to test
+	 * 
+	 * @param expectedValidCommand
+	 * 			The expected valid command to be returned
+	 */
+	private void getCommandValidTest(String validCommand, 
+			Controller.Commands expectedValidCommand) {
+		setUpTestController(validCommand);
+		Vector<Pair<Parser.Token, String>> validCommandTokens = 
+				generateListOfTokens(validCommand);
+		
+		assertEquals(expectedValidCommand, controller.getCommand(validCommandTokens));
+	}
+	
+	/**
+	 * Test for exceptions thrown by the getCommand method
+	 * 
+	 * @param invalidCommand 
+	 * 			The invalid command to test
+	 * 
+	 * @param expectedErrorMsg 
+	 * 			The expected error message to be thrown
+	 */
+	private void getCommandExceptionTest(String invalidCommand, 
+			String expectedErrorMsg) {
+		setUpTestController(invalidCommand);
+		Vector<Pair<Parser.Token, String>> invalidCommandTokens = 
+				generateListOfTokens(invalidCommand);
+		
+		try {
+			controller.getCommand(invalidCommandTokens);
+			fail("The invalid command " + invalidCommand + " for getCommandExceptionTest "
+				 + "did not throw the exception message '" + expectedErrorMsg + "'");
+		} catch (Error error) {
+			assertEquals(expectedErrorMsg, error.getMessage());
+		}
+	}
+	
+	/**
+	 * Tests the startCommandExecution method
+	 */
 	@Test
 	public void startCommandExecutionTest() {
 		String invalidCommand = "Addsz";
@@ -81,7 +127,6 @@ public class ControllerTest {
 			controller.startCommandExecution();
 			fail("startCommandExecutionTest - invalidCommand exception not thrown");
 		} catch (Error error) {
-			System.out.println(error.getMessage());
 			assertEquals(expectedUnrecognisedCommand, error.getMessage());
 		}
 	}
