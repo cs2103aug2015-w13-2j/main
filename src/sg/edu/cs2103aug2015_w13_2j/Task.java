5f73cf906d540b45a5002d8b559fd639a553db9c
Chunqi Zhu <chunqi93@gmail.com>
2015-10-10 15:12:34 +0800

    Cleaned up Task toString and parseTask methods
    * toString() is @Override Object not a public interface method
    * parseTask() is static and thus should be defined in the interface and
    not in the implementing class

--- src/sg/edu/cs2103aug2015_w13_2j/Task.java
+++ src/sg/edu/cs2103aug2015_w13_2j/Task.java
@@ -1,11 +1,10 @@
 package sg.edu.cs2103aug2015_w13_2j;
 
-import java.lang.reflect.Method;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.Set;
-
 import java.util.Iterator;
+import java.util.Map.Entry;
+import java.util.Set;
 
 //@@author A0121410H
 
@@ -16,241 +15,216 @@ import java.util.Iterator;
  * @author Zhu Chunqi
  */
 public class Task implements TaskInterface {
-    // Maps labels to their values
-    private HashMap<String, String> mLabels = new HashMap<String, String>();
+	// Maps labels to their values
+	private HashMap<String, String> mLabels = new HashMap<String, String>();
 
-    /*******************************************************
+	/*******************************************************
 	 * CONSTRUCTORS
 	 *******************************************************/
-    
-    /**
-     * Zero parameter constructor that creates and initializes a new Task object
-     * and records the time of creation
-     */
-    public Task() {
-        Date now = new Date();
-        setCreated(String.valueOf(now.getTime()));
-        setCompleted("FALSE");
-        setArchived("FALSE");
-        setImportant("FALSE");
-    }
 
-    /**
-     * Constructor to create a task with the name as provided. Internally
-     * creates a new Task object and calls setName(name)
-     * 
-     * @param name
-     *            The name to be given to the newly created task
-     */
-    public Task(String name) {
-        this();
-        setName(name);
-    }
-    
-    /*******************************************************
-	 * LABEL ACCESSORS
-	 * `-> NAME: string
-	 * `-> CREATED, START, END: date
-	 * `-> TYPE: EVENT / DEADLINE / FLOAT
-	 * `-> COMPLETED, ARCHIVED, IMPORTANT: TRUE / FALSE
+	/**
+	 * Zero parameter constructor that creates and initializes a new Task object
+	 * and records the time of creation
+	 */
+	public Task() {
+		Date now = new Date();
+		setCreated(String.valueOf(now.getTime()));
+		setCompleted("FALSE");
+		setArchived("FALSE");
+		setImportant("FALSE");
+	}
+
+	/**
+	 * Constructor to create a task with the name as provided. Internally
+	 * creates a new Task object and calls setName(name)
+	 * 
+	 * @param name
+	 *            The name to be given to the newly created task
+	 */
+	public Task(String name) {
+		this();
+		setName(name);
+	}
+
+	/*******************************************************
+	 * LABEL ACCESSORS `-> NAME: string `-> CREATED, START, END: date `-> TYPE:
+	 * EVENT / DEADLINE / FLOAT `-> COMPLETED, ARCHIVED, IMPORTANT: TRUE / FALSE
 	 *******************************************************/
-    
-    public void setLabel(String label, String value) {
-        mLabels.put(label, value);
-    }
 
-    public String getLabel(String label) {
-        return mLabels.get(label);
-    }
-    
-    /*******************************************************
+	public void setLabel(String label, String value) {
+		mLabels.put(label, value);
+	}
+
+	public String getLabel(String label) {
+		return mLabels.get(label);
+	}
+
+	/*******************************************************
 	 * (ACCESSORS) NAME: string
 	 *******************************************************/
 
-    public void setName(String name) {
-        setLabel("NAME", name);
-    }
+	public void setName(String name) {
+		setLabel("NAME", name);
+	}
 
-    public String getName() {
-        return getLabel("NAME");
-    }
+	public String getName() {
+		return getLabel("NAME");
+	}
 
-    /*******************************************************
-   	 * (ACCESSORS) CREATED, START, END: date
-   	 *******************************************************/
+	/*******************************************************
+	 * (ACCESSORS) CREATED, START, END: date
+	 *******************************************************/
 
-    public void setCreated(String createdString) {
-    	setLabel("CREATED", createdString);
-    }
+	public void setCreated(String createdString) {
+		setLabel("CREATED", createdString);
+	}
 
-    public Date getCreated() {
-        return stringToDate(getLabel("CREATED"));
-    }
+	public Date getCreated() {
+		return stringToDate(getLabel("CREATED"));
+	}
 
-    public void setStart(Date start) {
-    	setLabel("START", dateToString(start));
-    }
-  
-    public void setStart(String startString) {
-    	setLabel("START", startString);
-    }
-    
-    public Date getStart() {
-    	return stringToDate(getLabel("START"));
-    }
+	public void setStart(Date start) {
+		setLabel("START", dateToString(start));
+	}
 
-    public void setEnd(Date end) {
-        setLabel("END", dateToString(end));
-    }
+	public void setStart(String startString) {
+		setLabel("START", startString);
+	}
 
-    public void setEnd(String endString) {
-    	setLabel("END", endString);
-    }
-    
-    public Date getEnd() {
-        return stringToDate(getLabel("END"));
-    }
-    
-    /**
-     * Utility method to convert a String millisecond epoch to a Date object
-     * 
-     * @param date
-     *            String representing the millisecond epoch
-     * @return Date object created from the millisecond epoch or null if string
-     *         passed is not a valid long or is null
-     */
-    private Date stringToDate(String date) {
-        if (date == null) {
-            return null;
-        } else {
-            try {
-                return new Date(Long.parseLong(date));
-            } catch (NumberFormatException e) {
-                return null;
-            }
-        }
-    }
+	public Date getStart() {
+		return stringToDate(getLabel("START"));
+	}
 
-    /**
-     * Utility function to convert a Date object to String representing the
-     * millisecond epoch
-     * 
-     * @param date
-     *            Date object to be converted
-     * @return String object representing the date as the millisecond epoch or
-     *         null if the provided date object is null
-     */
-    private String dateToString(Date date) {
-        if (date == null) {
-            return null;
-        } else {
-            return String.valueOf(date.getTime());
-        }
-    }
-    
-    /*******************************************************
-   	 * (ACCESSORS) TYPE: EVENT / DEADLINE / FLOAT
-   	 *******************************************************/
-    
-    public void setType(String typeString) {
-    	switch(typeString) {
-    		case "EVENT":
-    		case "DEADLINE":
-    		case "FLOAT":
-    			setLabel("TYPE", typeString);
-    			break;
-    	}
-    }
+	public void setEnd(Date end) {
+		setLabel("END", dateToString(end));
+	}
 
-    public String getType() {
-        return getLabel("TYPE");
-    }
+	public void setEnd(String endString) {
+		setLabel("END", endString);
+	}
 
-    /*******************************************************
-   	 * (ACCESSORS) COMPLETED, ARCHIVED, IMPORTANT: TRUE / FALSE
-   	 *******************************************************/
-    
-    // COMPLETED
-    public void setCompleted(String completedString) {
-    	switch(completedString) {
-    		case "TRUE":
-    		case "FALSE":
-    			setLabel("COMPLETED", completedString);
-    			break;
-    	}
-    }
-    
-    public String getCompleted() {
-        return getLabel("COMPLETED");
-    }
-    
-    // ARCHIVED
-    public void setArchived(String archivedString) {
-    	switch(archivedString) {
-    		case "TRUE":
-    		case "FALSE":
-    			setLabel("ARCHIVED", archivedString);
-    			break;
-    	}
-    }
-    
-    public String getArchived() {
-        return getLabel("ARCHIVED");
-    }
-    
-    // IMPORTANT
-    public void setImportant(String importantString) {
-    	switch(importantString) {
-    		case "TRUE":
-    		case "FALSE":
-    			setLabel("IMPORTANT", importantString);
-    			break;
-    	}
-    }
-    
-    public String getImportant() {
-        return getLabel("IMPORTANT");
-    }
-    
-    /*******************************************************
-   	 * STRING <-> TASK CONVERSION METHODS
-   	 *******************************************************/
-    
-    //@@author A0124007X
-    public String toString() {
-    	String output = "";
-    	
-        Set<String> labelSet = mLabels.keySet();
-        Iterator<String> labelIter = labelSet.iterator();
-        
-        while(labelIter.hasNext()){
-            String label = labelIter.next();
-            String value = mLabels.get(label);
-            output += label + ":" + value + "\n";
-        }
-        
-        return output;
-    }
-    
-    public static Task parseTask(String taskString) throws Exception {
-    	Task task = new Task();
-        
-        String[] attributes = taskString.split("\r|\n");
-        for(String pair : attributes) {
-            if(pair.isEmpty()) {
-                continue;
-            } else {
-                // Only splits by the first colon
-                String[] pairArray = pair.split(":", 2);
-                String labelName = pairArray[0].toLowerCase();
-                labelName = labelName.substring(0,1).toUpperCase() + labelName.substring(1);
-                String value = pairArray[1];
+	public Date getEnd() {
+		return stringToDate(getLabel("END"));
+	}
 
-                Method m = Task.class.getMethod("set" + labelName, String.class);
-                m.invoke(task, value);
-            }
-        }
-        
-        return task;
-    }
+	/**
+	 * Utility method to convert a String millisecond epoch to a Date object
+	 * 
+	 * @param date
+	 *            String representing the millisecond epoch
+	 * @return Date object created from the millisecond epoch or null if string
+	 *         passed is not a valid long or is null
+	 */
+	private Date stringToDate(String date) {
+		if (date == null) {
+			return null;
+		} else {
+			try {
+				return new Date(Long.parseLong(date));
+			} catch (NumberFormatException e) {
+				return null;
+			}
+		}
+	}
+
+	/**
+	 * Utility function to convert a Date object to String representing the
+	 * millisecond epoch
+	 * 
+	 * @param date
+	 *            Date object to be converted
+	 * @return String object representing the date as the millisecond epoch or
+	 *         null if the provided date object is null
+	 */
+	private String dateToString(Date date) {
+		if (date == null) {
+			return null;
+		} else {
+			return String.valueOf(date.getTime());
+		}
+	}
+
+	/*******************************************************
+	 * (ACCESSORS) TYPE: EVENT / DEADLINE / FLOAT
+	 *******************************************************/
+
+	public void setType(String typeString) {
+		switch (typeString) {
+		case "EVENT":
+		case "DEADLINE":
+		case "FLOAT":
+			setLabel("TYPE", typeString);
+			break;
+		}
+	}
+
+	public String getType() {
+		return getLabel("TYPE");
+	}
+
+	/*******************************************************
+	 * (ACCESSORS) COMPLETED, ARCHIVED, IMPORTANT: TRUE / FALSE
+	 *******************************************************/
+
+	// COMPLETED
+	public void setCompleted(String completedString) {
+		switch (completedString) {
+		case "TRUE":
+		case "FALSE":
+			setLabel("COMPLETED", completedString);
+			break;
+		}
+	}
+
+	public String getCompleted() {
+		return getLabel("COMPLETED");
+	}
+
+	// ARCHIVED
+	public void setArchived(String archivedString) {
+		switch (archivedString) {
+		case "TRUE":
+		case "FALSE":
+			setLabel("ARCHIVED", archivedString);
+			break;
+		}
+	}
+
+	public String getArchived() {
+		return getLabel("ARCHIVED");
+	}
+
+	// IMPORTANT
+	public void setImportant(String importantString) {
+		switch (importantString) {
+		case "TRUE":
+		case "FALSE":
+			setLabel("IMPORTANT", importantString);
+			break;
+		}
+	}
+
+	public String getImportant() {
+		return getLabel("IMPORTANT");
+	}
+
+	// @@author A0124007X
+	/**
+	 * Converts the Task object into string format for storage
+	 * 
+	 * @return String representing the Task object
+	 */
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		Iterator<Entry<String, String>> iter = mLabels.entrySet().iterator();
+		while (iter.hasNext()) {
+			Entry<String, String> entry = iter.next();
+			sb.append(entry.getKey());
+			sb.append(":");
+			sb.append(entry.getValue());
+			sb.append("\n");
+		}
+		return sb.toString();
+	}
 }
--- src/sg/edu/cs2103aug2015_w13_2j/TaskInterface.java
+++ src/sg/edu/cs2103aug2015_w13_2j/TaskInterface.java
@@ -1,223 +1,186 @@
 package sg.edu.cs2103aug2015_w13_2j;
 
+import java.lang.reflect.Method;
 import java.util.Date;
 
 //@@author A0121410H
 
-/**
- * Public interface of the Task class
- * 
- * @author Zhu Chunqi
- */
 public interface TaskInterface {
-	/*******************************************************
-	 * AUTHORISED VALUES
-	 * `-> NAME: string
-	 * `-> CREATED, START, END: date
-	 * `-> TYPE: EVENT / DEADLINE / FLOAT
-	 * `-> COMPLETED, ARCHIVED, IMPORTANT: TRUE / FALSE
-	 *******************************************************/
-	
-	/*
-    // Enumerated labels
-    public enum Label {
-        NAME, CREATED, START, END, TYPE, COMPLETED, ARCHIVED, IMPORTANT
-    }
-    
-    //@@author A0133387B
-    public enum Type {
-    	// EVENT: START + END
-    	// DEADLINE: END
-    	// FLOAT: N/A
-        EVENT, DEADLINE, FLOAT
-    }
-    
-    //@@author A0124007X
-    public enum Completed {
-    	TRUE, FALSE
-    }
-    public enum Archived {
-    	TRUE, FALSE
-    }
-    
-    public enum Important {
-    	TRUE, FALSE
-    }
-    */
-    
-    /*******************************************************
-	 * LABEL ACCESSORS
-	 *******************************************************/
-    
-    /**
-     * Sets a label-value pair of the Task object
-     * 
-     * @param label
-     *            The label to be set
-     * @param value
-     *            The value to set the label to or null to unset the label
-     */
-    public void setLabel(String label, String value);
+	/**
+	 * Sets a label-value pair of the Task object
+	 * 
+	 * @param label
+	 *            The label to be set
+	 * @param value
+	 *            The value to set the label to or null to unset the label
+	 */
+	public void setLabel(String label, String value);
 
-    /**
-     * Retrieves the value of a label of the Task object
-     * 
-     * @return The value of the label or null if not set
-     */
-    public String getLabel(String label);
-    
-    /*******************************************************
+	/**
+	 * Retrieves the value of a label of the Task object
+	 * 
+	 * @return The value of the label or null if not set
+	 */
+	public String getLabel(String label);
+
+	/*******************************************************
 	 * (ACCESSORS) NAME: string
 	 *******************************************************/
-    
-    /**
-     * Sets the name of this Task object as a label-value pair
-     * 
-     * @param name
-     *            The string to set the task's name to
-     */
-    public void setName(String name);
-    
-    /**
-     * Retrieves the name label of this Task object
-     * 
-     * @return The name of the Task object or null if not set
-     */
-    public String getName();
-    
-    /*******************************************************
-   	 * (ACCESSORS) CREATED, START, END: date
-   	 *******************************************************/
-    
-    /**
-     * Sets the date and time the Task object was created.
-     * Used only in parseTask()
-     * 
-     * @param createdString
-     *            String representing the millisecond epoch
-     */
-    public void setCreated(String createdString);
-    
-    /**
-     * Convenience method to retrieve the date and time this Task object was
-     * created in a Date object
-     * 
-     * @return Date object representing the date and time created
-     */
-    public Date getCreated();
 
-    /**
-     * Sets the start date of the Task object
-     * 
-     * @param start
-     *            Date object representing the start date or null to unset
-     */
-    public void setStart(Date start);
-    
-    /**
-     * Sets the start date of the Task object
-     * 
-     * @param startString
-     *            String representing the millisecond epoch
-     */
-    public void setStart(String startString);
+	/**
+	 * Sets the name of this Task object as a label-value pair
+	 * 
+	 * @param name
+	 *            The string to set the task's name to
+	 */
+	public void setName(String name);
 
-    /**
-     * Retrieves the start date of the Task object
-     * 
-     * @return Date object representing the start date or null if not set
-     */
-    public Date getStart();
-    
-    /**
-     * Sets the end date of the Task object
-     * 
-     * @param end
-     *            Date object representing the end date or null to unset
-     */
-    public void setEnd(Date end);
-    
-    /**
-     * Sets the end date of the Task object
-     * 
-     * @param endString
-     *            String representing the millisecond epoch
-     */
-    public void setEnd(String endString);
+	/**
+	 * Retrieves the name label of this Task object
+	 * 
+	 * @return The name of the Task object or null if not set
+	 */
+	public String getName();
 
-    /**
-     * Retrieves the end date of the Task object
-     * 
-     * @return Date object representing the end date or null if not set
-     */
-    public Date getEnd();
-    
-    /*******************************************************
-   	 * (ACCESSORS) TYPE: EVENT / DEADLINE / FLOAT
-   	 *******************************************************/
-    
-    /**
-     * Categorizes a task into one of the 3 types: an Event, Deadline, or Floating Task
-     * @param typeString
-     *            one of 3 types of tasks: Event / Deadline / Float
-     * @author Nguyen Tuong Van
-     * 
-    */
-    public void setType(String typeString);
-    
-    public String getType();
-    
-    /*******************************************************
-   	 * (ACCESSORS) COMPLETED, ARCHIVED, IMPORTANT: TRUE / FALSE
-   	 *******************************************************/
-    
-    /**
-     * Sets true or false for a property of the task
-     * @param propertyString
-     *            where property is completed or archived or important
-     * 
-     * Gets the flag for a property of the task
-     * @return String representing true or false for a property of the task
-    */
-    
-    // COMPLETED
-    public void setCompleted(String completedString);
-    
-    public String getCompleted();
-    
-    // ARCHIVED
-    public void setArchived(String archivedString);
-    
-    public String getArchived();
-    
-    // IMPORTANT
-    public void setImportant(String importantString);
-    
-    public String getImportant();
-    
-    /*******************************************************
-   	 * STRING <-> TASK CONVERSION METHODS
-   	 *******************************************************/
-    
-    //@@author A0124007X
-    /**
-     * Converts Task object into string format for storage in the data file
-     * 
-     * @return String representing the Task and its attributes
-     */
-    public String toString();
-    
-    /**
-     * Converts a string into its corresponding Task object
-     * Note: Exception must be thrown in all methods that call parseTask()
-     * 
-     * @param taskString
-     *            A Task in its string format
-     * @return The corresponding Task object after conversion
-     */
-    public static Task parseTask(String taskString) throws Exception {
-    	Task task = new Task();
-    	// To be overridden in Task.java
-        return task;
-    }
+	/*******************************************************
+	 * (ACCESSORS) CREATED, START, END: date
+	 *******************************************************/
+
+	/**
+	 * Sets the date and time the Task object was created. Used only in
+	 * parseTask()
+	 * 
+	 * @param createdString
+	 *            String representing the millisecond epoch
+	 */
+	public void setCreated(String createdString);
+
+	/**
+	 * Convenience method to retrieve the date and time this Task object was
+	 * created in a Date object
+	 * 
+	 * @return Date object representing the date and time created
+	 */
+	public Date getCreated();
+
+	/**
+	 * Sets the start date of the Task object
+	 * 
+	 * @param start
+	 *            Date object representing the start date or null to unset
+	 */
+	public void setStart(Date start);
+
+	/**
+	 * Sets the start date of the Task object
+	 * 
+	 * @param startString
+	 *            String representing the millisecond epoch
+	 */
+	public void setStart(String startString);
+
+	/**
+	 * Retrieves the start date of the Task object
+	 * 
+	 * @return Date object representing the start date or null if not set
+	 */
+	public Date getStart();
+
+	/**
+	 * Sets the end date of the Task object
+	 * 
+	 * @param end
+	 *            Date object representing the end date or null to unset
+	 */
+	public void setEnd(Date end);
+
+	/**
+	 * Sets the end date of the Task object
+	 * 
+	 * @param endString
+	 *            String representing the millisecond epoch
+	 */
+	public void setEnd(String endString);
+
+	/**
+	 * Retrieves the end date of the Task object
+	 * 
+	 * @return Date object representing the end date or null if not set
+	 */
+	public Date getEnd();
+
+	/*******************************************************
+	 * (ACCESSORS) TYPE: EVENT / DEADLINE / FLOAT
+	 *******************************************************/
+
+	/**
+	 * Categorizes a task into one of the 3 types: an Event, Deadline, or
+	 * Floating Task
+	 * 
+	 * @param typeString
+	 *            one of 3 types of tasks: Event / Deadline / Float
+	 * @author Nguyen Tuong Van
+	 * 
+	 */
+	public void setType(String typeString);
+
+	public String getType();
+
+	/*******************************************************
+	 * (ACCESSORS) COMPLETED, ARCHIVED, IMPORTANT: TRUE / FALSE
+	 *******************************************************/
+
+	/**
+	 * Sets true or false for a property of the task
+	 * 
+	 * @param propertyString
+	 *            where property is completed or archived or important
+	 * 
+	 *            Gets the flag for a property of the task
+	 * @return String representing true or false for a property of the task
+	 */
+
+	// COMPLETED
+	public void setCompleted(String completedString);
+
+	public String getCompleted();
+
+	// ARCHIVED
+	public void setArchived(String archivedString);
+
+	public String getArchived();
+
+	// IMPORTANT
+	public void setImportant(String importantString);
+
+	public String getImportant();
+
+	/*******************************************************
+	 * STRING <-> TASK CONVERSION METHODS
+	 *******************************************************/
+
+	// @@author A0124007X
+	/**
+	 * Converts a string into its corresponding Task object
+	 * 
+	 * @param taskString
+	 *            The string representing a Task object
+	 * @return The constructed Task object
+	 */
+	public static Task parseTask(String taskString) {
+		Task task = new Task();
+		String[] attributes = taskString.split("\r|\n");
+		for (String pair : attributes) {
+			if (!pair.isEmpty()) {
+				// Only splits by the first colon
+				String[] pairTokens = pair.split(":", 2);
+				if (pairTokens.length == 2) {
+					String label = pairTokens[0].toUpperCase();
+					String value = pairTokens[1];
+					task.setLabel(label, value);
+				}
+			}
+		}
+		return task;
+	}
 }
