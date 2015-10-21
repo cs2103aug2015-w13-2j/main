051ad7b6765aada86621cad02c57fc81a081a2d9
Chunqi Zhu <chunqi93@gmail.com>
2015-10-21 12:39:10 +0800

    Removed unused command handlers

--- src/sg/edu/cs2103aug2015_w13_2j/commands/RetrieveHandler.java
+++ src/sg/edu/cs2103aug2015_w13_2j/commands/RetrieveHandler.java
@@ -14,21 +14,21 @@ import sg.edu.cs2103aug2015_w13_2j.ui.FeedbackMessage;
 import sg.edu.cs2103aug2015_w13_2j.ui.FeedbackMessage.FeedbackType;
 
 /**
- * Unarchives a Task specified by the user. 
+ * Unarchives a Task specified by the user.
  * 
- * This class includes an execution method that sets the "ARCHIVED" label 
- * of the Task to false. A user feedback message will subsequently be 
- * returned upon setting the label. If the Task index specified is 
- * out of range, or does not exist, a user error message will be returned.
+ * This class includes an execution method that sets the "ARCHIVED" label of the
+ * Task to false. A user feedback message will subsequently be returned upon
+ * setting the label. If the Task index specified is out of range, or does not
+ * exist, a user error message will be returned.
  * 
  * @author Natasha Koh Sze Sze
  */
 public class RetrieveHandler extends CommandHandler {
-    private static final Logger LOGGER = Logger
-            .getLogger(SearchHandler.class.getName());
+    private static final Logger LOGGER = Logger.getLogger(RetrieveHandler.class
+            .getName());
     private static final String[] RESERVED = { "retrieve", "ret" };
     private static final String RETRIEVE_SUCCESS = "Task retrieved successfully.";
-    
+
     @Override
     public FeedbackMessage execute(ArrayList<Pair<Token, String>> tokens) {
         for (Pair<Token, String> pair : tokens) {
@@ -38,7 +38,8 @@ public class RetrieveHandler extends CommandHandler {
                             Integer.parseInt(pair.getValue()));
                     task.setArchived(false);
                     logRetrievedTask(task);
-                    return new FeedbackMessage(RETRIEVE_SUCCESS, FeedbackType.INFO);
+                    return new FeedbackMessage(RETRIEVE_SUCCESS,
+                            FeedbackType.INFO);
                 } catch (TaskNotFoundException e) {
                     return FeedbackMessage.getTaskNotFoundError();
                 }
@@ -46,16 +47,16 @@ public class RetrieveHandler extends CommandHandler {
         }
         return FeedbackMessage.getTaskNotFoundError();
     }
-    
+
     @Override
     public List<String> getReservedKeywords() {
         return Arrays.asList(RESERVED);
     }
-    
+
     private void logRetrievedTask(Task retrievedTask) {
         String nameOfArchivedTask = retrievedTask.getName();
-        
-        LOGGER.info("[CommandHandler][RetrieveHandler] '" + nameOfArchivedTask 
-                    + "' archived status is: " + retrievedTask.isArchived());
+
+        LOGGER.info("[CommandHandler][RetrieveHandler] '" + nameOfArchivedTask
+                + "' archived status is: " + retrievedTask.isArchived());
     }
 }
