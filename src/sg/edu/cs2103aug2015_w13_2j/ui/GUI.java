3e22615fa0a98c744693d901c94da58c3786a769
Yang Kenneth Lu <lu.yang.kenneth@gmail.com>
2015-10-23 15:32:43 +0800

    Added style.css
    WIP #96

--- src/sg/edu/cs2103aug2015_w13_2j/ui/GUI.java
+++ src/sg/edu/cs2103aug2015_w13_2j/ui/GUI.java
@@ -8,15 +8,20 @@ import javafx.stage.Stage;
 
 public class GUI extends Application {
 	@Override
-	public void start(Stage stage) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource("GUI.fxml"));
-		Scene scene = new Scene(root);
-		
-		// TODO: app icons in window and in taskbar
-		stage.setTitle("FunDUE");
-		stage.setScene(scene);
-		stage.setResizable(false);
-		stage.show();
+	public void start(Stage stage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("GUI.fxml"));
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+			
+			// TODO: app icons in window and in taskbar
+			stage.setTitle("FunDUE");
+			stage.setScene(scene);
+			stage.setResizable(false);
+			stage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public static void main(String[] args) {
--- src/sg/edu/cs2103aug2015_w13_2j/ui/GUIController.java
+++ src/sg/edu/cs2103aug2015_w13_2j/ui/GUIController.java
@@ -3,11 +3,17 @@ package sg.edu.cs2103aug2015_w13_2j.ui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 public class GUIController implements Initializable {
+	@FXML
+	public void doSomething() {
+		// Do nothing
+	}
+	
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
-		// Do nothing
+		doSomething();
 	}
 }
