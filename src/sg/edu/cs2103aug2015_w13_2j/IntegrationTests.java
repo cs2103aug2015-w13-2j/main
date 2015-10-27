47e5e4bbece34397ac527242600747c825f84aa9
SmokinClove <van.tuong.nguyen@hotmail.com>
2015-10-28 03:22:14 +0800

    Added some more tests for the methods modifying the tasks and filter
    WIP #127

--- src/sg/edu/cs2103aug2015_w13_2j/IntegrationTests.java
+++ src/sg/edu/cs2103aug2015_w13_2j/IntegrationTests.java
@@ -10,6 +10,11 @@ import org.junit.Test;
 import sg.edu.cs2103aug2015_w13_2j.TaskInterface.TaskNotFoundException;
 import sg.edu.cs2103aug2015_w13_2j.commands.AddHandler;
 import sg.edu.cs2103aug2015_w13_2j.commands.ArchiveHandler;
+import sg.edu.cs2103aug2015_w13_2j.commands.DeleteHandler;
+import sg.edu.cs2103aug2015_w13_2j.commands.EditHandler;
+import sg.edu.cs2103aug2015_w13_2j.commands.FilterHandler;
+import sg.edu.cs2103aug2015_w13_2j.commands.MarkImportantHandler;
+import sg.edu.cs2103aug2015_w13_2j.commands.UnarchiveHandler;
 import sg.edu.cs2103aug2015_w13_2j.ui.TextUIStub;
 
 public class IntegrationTests {
@@ -21,6 +26,11 @@ public class IntegrationTests {
     public static void setup() {
         sLogic.registerCommandHandler(new AddHandler());
         sLogic.registerCommandHandler(new ArchiveHandler());
+        sLogic.registerCommandHandler(new UnarchiveHandler());
+        sLogic.registerCommandHandler(new EditHandler());
+        sLogic.registerCommandHandler(new FilterHandler());
+        sLogic.registerCommandHandler(new DeleteHandler());
+        sLogic.registerCommandHandler(new MarkImportantHandler());
         sLogic.injectDependencies(sStorage, sTextUI);
         sLogic.readTasks();
     }
@@ -47,9 +57,36 @@ public class IntegrationTests {
         // FilterChain should still be /all/
         assertEquals("/all/", sTextUI.getFilterChain());
         
+        //Testing various methods that modify the task to ensure Logic works well with Storage
         assertEquals(sLogic.getTask(0).isArchived(), false);
         int indexToArchive = 0;
         sLogic.executeCommand("archive " + indexToArchive);
+        // Task should have been modified in Storage component
         assertEquals(sStorage.readTasksFromDataFile().get(0).isArchived(), true); 
+        
+        sLogic.executeCommand("unarchive " + indexToArchive);
+        assertEquals(sStorage.readTasksFromDataFile().get(0).isArchived(), false); 
+        
+        sLogic.executeCommand("edit " + indexToArchive + " -e 27/10");
+        assertEquals(sStorage.readTasksFromDataFile().get(0).isOverdue(), true); 
+        assertEquals(sStorage.readTasksFromDataFile().get(0).getType(), "DEADLINE"); 
+        
+        sLogic.executeCommand("! " + indexToArchive);
+        assertEquals(sStorage.readTasksFromDataFile().get(0).isImportant(), true);
+        
+        sLogic.executeCommand("del " + indexToArchive);
+        assertEquals(sStorage.readTasksFromDataFile().size(), 0);
+        //Test again to make sure Logic works well with TextUI
+        assertEquals(0, sTextUI.getTasksForDisplay().size());
+        
+    }
+    
+    @Test
+    public void testFilter(){
+    	sLogic.executeCommand("add '" + "first task" + "'");
+    	sLogic.executeCommand("! 0");
+    	sLogic.executeCommand("filter is:important");
+        assertEquals(sTextUI.getFilterChain(), "/all/is:important/");
+        assertEquals(sStorage.readTasksFromDataFile().get(0).isImportant(), true); 
     }
 }
