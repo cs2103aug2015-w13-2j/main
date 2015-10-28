2466abe27fea0f254cbe1a7e4c2fb9439b0a04b5
Yang Kenneth Lu <lu.yang.kenneth@gmail.com>
2015-10-28 15:45:04 +0800

    Changed GUI to HTML implementation instead of FXML
    * Can run Javascript from Java
    * Can call Java methods from Javascript
    * WIP #96
    * Lunch with Van

--- src/sg/edu/cs2103aug2015_w13_2j/ui/GUI.java
+++ src/sg/edu/cs2103aug2015_w13_2j/ui/GUI.java
@@ -1,23 +1,62 @@
 package sg.edu.cs2103aug2015_w13_2j.ui;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Worker.State;
 import javafx.scene.Scene;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 import javafx.stage.Stage;
+import netscape.javascript.JSObject;
 
 public class GUI extends Application {
+	private WebView browser;
+	private WebEngine webEngine;
+	
+	public void exit() {
+		Platform.exit();
+	}
+	
+	private void initializeGUI() {
+		// Allows Java upcalls from Javascript
+		JSObject window = (JSObject) js("window");
+		window.setMember("GUI", new GUI());
+		
+		js("document.getElementById('view').innerHTML = 'testing'");
+		js("document.getElementById('view').setAttribute('onclick', 'GUI.exit()')");
+	}
+	
+	// Convenience method to run Javascript commands
+	private Object js(String command) {
+		return webEngine.executeScript(command);
+	}
+	
 	@Override
 	public void start(Stage stage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("GUI.fxml"));
-			Scene scene = new Scene(root);
-			scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+			browser = new WebView();
+			Scene scene = new Scene(browser);
+			webEngine = browser.getEngine();
+			webEngine.load(getClass().getResource("GUI.html").toExternalForm());
+			
+			webEngine.getLoadWorker().stateProperty().addListener(
+				new ChangeListener<State>(){
+					public void changed(ObservableValue ov, State oldState, State newState) {
+						if (newState == State.SUCCEEDED) {
+							initializeGUI();
+						}
+					}
+				}
+			);
 			
 			// TODO: app icons in window and in taskbar
 			stage.setTitle("FunDUE");
-			stage.setScene(scene);
+			stage.setHeight(800);
+			stage.setWidth(700);
 			stage.setResizable(false);
+			stage.setScene(scene);
 			stage.show();
 		} catch (Exception e) {
 			e.printStackTrace();
