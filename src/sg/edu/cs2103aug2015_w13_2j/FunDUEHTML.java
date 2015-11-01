ac6098c6c2867a9ddd8d003520d4bd171b170db9
Yang Kenneth Lu <lu.yang.kenneth@gmail.com>
2015-11-02 03:25:30 +0800

    Tried swapping TextUI with GUI
    * There are a 3 bugs that prevent FunDUEHTML from working properly
    * @chunqi please help
    * WIP #96

---
+++ src/sg/edu/cs2103aug2015_w13_2j/FunDUEHTML.java
@@ -0,0 +1,60 @@
+package sg.edu.cs2103aug2015_w13_2j;
+
+import sg.edu.cs2103aug2015_w13_2j.commands.AddHandler;
+//import sg.edu.cs2103aug2015_w13_2j.commands.ArchiveHandler;
+import sg.edu.cs2103aug2015_w13_2j.commands.DeleteHandler;
+import sg.edu.cs2103aug2015_w13_2j.commands.EditHandler;
+import sg.edu.cs2103aug2015_w13_2j.commands.ExitHandler;
+import sg.edu.cs2103aug2015_w13_2j.commands.FilterHandler;
+import sg.edu.cs2103aug2015_w13_2j.commands.HelpHandler;
+import sg.edu.cs2103aug2015_w13_2j.commands.LoadHandler;
+import sg.edu.cs2103aug2015_w13_2j.commands.MarkCompletedHandler;
+import sg.edu.cs2103aug2015_w13_2j.commands.MarkImportantHandler;
+import sg.edu.cs2103aug2015_w13_2j.commands.PopHandler;
+import sg.edu.cs2103aug2015_w13_2j.commands.SearchHandler;
+//import sg.edu.cs2103aug2015_w13_2j.commands.UnarchiveHandler;
+import sg.edu.cs2103aug2015_w13_2j.commands.UndoHandler;
+import sg.edu.cs2103aug2015_w13_2j.storage.Storage;
+import sg.edu.cs2103aug2015_w13_2j.ui.GUI;
+
+// @@author A0121410H
+
+public class FunDUEHTML {
+    private GUI mGUI;
+    private Storage mStorage = Storage.getInstance();
+    private Logic mLogic = Logic.getInstance();
+
+    /**
+     * Initialization of components. Components are <b>not</b> allowed to
+     * reference other components during initialization. Additional
+     * initialization should be called from the {@link #run()} method instead
+     */
+    public FunDUEHTML() {
+        mLogic.registerCommandHandler(new AddHandler());
+        mLogic.registerCommandHandler(new EditHandler());
+        mLogic.registerCommandHandler(new DeleteHandler());
+        //mLogic.registerCommandHandler(new ArchiveHandler());
+        //mLogic.registerCommandHandler(new UnarchiveHandler());
+        mLogic.registerCommandHandler(new MarkImportantHandler());
+        mLogic.registerCommandHandler(new MarkCompletedHandler());
+        mLogic.registerCommandHandler(new UndoHandler());
+        mLogic.registerCommandHandler(new FilterHandler());
+        mLogic.registerCommandHandler(new PopHandler());
+        mLogic.registerCommandHandler(new HelpHandler());
+        mLogic.registerCommandHandler(new ExitHandler());
+        mLogic.registerCommandHandler(new LoadHandler());
+        mLogic.registerCommandHandler(new SearchHandler());
+    }
+
+    public void run() {
+        mGUI = GUI.getInstance();
+        mGUI.injectDependency(mLogic);
+        mLogic.injectDependencies(mStorage, mGUI);
+        mLogic.readTasks();
+        mLogic.display();
+    }
+
+    public static void main(String[] args) {
+        new FunDUEHTML().run();
+    }
+}
--- src/sg/edu/cs2103aug2015_w13_2j/ui/GUI.java
+++ src/sg/edu/cs2103aug2015_w13_2j/ui/GUI.java
@@ -1,7 +1,10 @@
 package sg.edu.cs2103aug2015_w13_2j.ui;
 
+import java.util.ArrayList;
+
+import org.ocpsoft.prettytime.PrettyTime;
+
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.concurrent.Worker.State;
@@ -12,61 +15,201 @@ import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 import netscape.javascript.JSObject;
 
-public class GUI extends Application {
-	private WebView browser;
-	private WebEngine webEngine;
-	
-	public void executeCommand(String x) {
-	    // TODO
-	    System.out.println(x);
-	}
-	
-	private void initializeGUI() {
-		// Allows Java upcalls from Javascript
-		JSObject window = (JSObject) js("window");
-		window.setMember("GUI", new GUI());
-		
-		js("document.getElementById('commandForm').setAttribute('onsubmit', 'GUI.executeCommand(this.command.value)')");
-	}
-	
-	// Convenience method to run Javascript commands
-	private Object js(String command) {
-		return webEngine.executeScript(command);
-	}
-	
-	@Override
-	public void start(Stage stage) {
-		try {
-			browser = new WebView();
-			webEngine = browser.getEngine();
-			webEngine.load(getClass().getResource("GUI.html").toExternalForm());
-			
-			webEngine.getLoadWorker().stateProperty().addListener(
-				new ChangeListener<State>(){
-					public void changed(ObservableValue ov, State oldState, State newState) {
-						if (newState == State.SUCCEEDED) {
-							initializeGUI();
-						}
-					}
-				}
-			);
-			
-			// TODO: app icon and title in taskbar
-		    browser.setContextMenuEnabled(false);
-		    Scene scene = new Scene(browser);
-		    stage.setTitle("FunDUE");
-		    stage.getIcons().add(new Image("file:FunDUE Logo.png"));
-		    stage.setHeight(800);
-		    stage.setWidth(700);
-		    stage.setResizable(false);
-		    stage.setScene(scene);
-		    stage.show();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+import sg.edu.cs2103aug2015_w13_2j.LogicInterface;
+import sg.edu.cs2103aug2015_w13_2j.Task;
+import sg.edu.cs2103aug2015_w13_2j.TaskInterface.InvalidTaskException;
+import sg.edu.cs2103aug2015_w13_2j.TaskInterface.TaskNotFoundException;
+import sg.edu.cs2103aug2015_w13_2j.filters.Filter;
+import sg.edu.cs2103aug2015_w13_2j.filters.FilterChain;
+
+// @@author A0121410H
+
+public class GUI extends Application implements UIInterface {
+    public static final PrettyTime PRETTY_TIME = new PrettyTime();
+    private WebView browser;
+    private WebEngine webEngine;
+    
+    private static GUI sInstance;
+    
+    private final FilterChain mFilterChain;
+    
+    private ArrayList<Task> mTasks;
+    
+    private LogicInterface mLogic;
+    
+    /**
+     * Private constructor
+     */
+    // TODO: *BUG* cannot make this private, otherwise program crashes
+    public GUI() {
+        mTasks = new ArrayList<Task>();
+        mFilterChain = new FilterChain();
+    }
+    
+    /**
+     * Retrieves the singleton instance of this {@link UIInterface} component
+     * 
+     * @return {@link UIInterface} component singleton instance
+     */
+    public static GUI getInstance() {
+        if (sInstance == null) {
+            sInstance = new GUI();
+        }
+        return sInstance;
+    }
+    
+    @Override
+    public void injectDependency(LogicInterface logic) {
+        mLogic = logic;
+    }
+    
+    // @@author A0124007X
+    
+    @Override
+    public void display(ArrayList<Task> tasks) {
+        // TODO
+        // This is super primitive at the moment
+        
+        mTasks = tasks;
+        String s = "";
+        
+        if (tasks.size() > 0) {
+            for (int i = 0; i < tasks.size(); i++) {
+                Task task = tasks.get(i);
+                
+                try {
+                    if(task.isValid()){
+                        s += i + " " + task.getName() + "<br>";
+                    }
+                } catch (InvalidTaskException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+            
+            display(s);
+        } else {
+            display("No tasks!");
+        }
+    }
+    
+    @Override
+    public void display(String s) {
+        // TODO: *BUG* FunDUEHTML doesn't launch the app
+        //setContent("view", s);
+    }
+    
+    // @@author A0121410H
+    
+    @Override
+    public Task getTask(int index) throws TaskNotFoundException {
+        try {
+            // NOTE: list is zero indexed whereas display is 1 indexed
+            return mTasks.get(index - 1);
+        } catch (IndexOutOfBoundsException e) {
+            throw new TaskNotFoundException();
+        }
+    }
+    
+    @Override
+    public void feedback(FeedbackMessage f) {
+        setContent("feedbackContainer", f.getMessage());
+    }
+    
+    @Override
+    public void pushFilter(Filter filter) {
+        mFilterChain.pushFilter(filter);
+    }
+    
+    @Override
+    public Filter popFilter() {
+        return mFilterChain.popFilter();
+    }
+    
+    @Override
+    public void updateFilters(ArrayList<Task> tasks) {
+        mFilterChain.updateFilters(tasks);
+    }
+    
+    // @@author A0124007X
+    
+    /**
+     * Allows Java upcalls from Javascript
+     */
+    private void initializeGUI() {
+        JSObject window = (JSObject) js("window");
+        window.setMember("GUI", new GUI());
+        setAttribute("commandForm", "onsubmit", "GUI.executeCommand(this.command.value)");
+    }
+    
+    /**
+     * The only Java method that will be upcalled from Javascript
+     */
+    public void executeCommand(String command) {
+        //mLogic.executeCommand(command);
+
+        System.out.println("before command: " + command);
+        // TODO: *BUG* Javascript commands don't work within this method
+        setContent("view", command);
+        System.out.println("after command");
+    }
+
+    /**
+     * Convenience method to run Javascript commands
+     */
+    private Object js(String command) {
+        return webEngine.executeScript(command);
+    }
+    
+    /**
+     * Convenience method to set the attribute value of an HTML element
+     */
+    private void setAttribute(String id, String attribute, String value) {
+        js("document.getElementById('" + id + "').setAttribute('" + attribute + "', '"
+                + value + "');");
+    }
+    
+    /**
+     * Convenience method to write the contents of an HTML element
+     */
+    private void setContent(String id, String value) {
+        js("document.getElementById('" + id + "').innerHTML = '" + value + "';");
+    }
+    
+    @Override
+    public void start(Stage stage) {
+        try {
+            browser = new WebView();
+            webEngine = browser.getEngine();
+            webEngine.load(getClass().getResource("GUI.html").toExternalForm());
+            
+            webEngine.getLoadWorker().stateProperty().addListener(
+                new ChangeListener<State>(){
+                    public void changed(ObservableValue ov, State oldState,
+                            State newState) {
+                        if (newState == State.SUCCEEDED) {
+                            initializeGUI();
+                        }
+                    }
+                }
+            );
+            
+            // TODO: app icon and title in taskbar
+            browser.setContextMenuEnabled(false);
+            Scene scene = new Scene(browser);
+            stage.setTitle("FunDUE");
+            stage.getIcons().add(new Image("file:FunDUE Logo.png"));
+            stage.setHeight(800);
+            stage.setWidth(700);
+            stage.setResizable(false);
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
