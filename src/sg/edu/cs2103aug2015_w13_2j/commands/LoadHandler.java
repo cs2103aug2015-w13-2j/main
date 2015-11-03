e55d63d825134f183ef9694484558876dad99fc2
NatashaKSS <xnagol@gmail.com>
2015-11-03 21:57:17 +0800

    Added FeedbackMessage upon load success
    * Feedback message will, currently, just be displayed after
    logic.showChangeDataFilePathDialog(); is called.

--- src/sg/edu/cs2103aug2015_w13_2j/commands/LoadHandler.java
+++ src/sg/edu/cs2103aug2015_w13_2j/commands/LoadHandler.java
@@ -2,6 +2,8 @@ package sg.edu.cs2103aug2015_w13_2j.commands;
 
 import sg.edu.cs2103aug2015_w13_2j.Logic;
 import sg.edu.cs2103aug2015_w13_2j.parser.Command;
+import sg.edu.cs2103aug2015_w13_2j.ui.FeedbackMessage;
+import sg.edu.cs2103aug2015_w13_2j.ui.FeedbackMessage.FeedbackType;
 
 public class LoadHandler extends CommandHandler {
     private static final String NAME = "Load Data File";
@@ -9,6 +11,8 @@ public class LoadHandler extends CommandHandler {
     private static final String[] FLAGS = {};
     private static final String[] OPTIONS = {};
     private static final String[] RESERVED = { "load" };
+    private static final String LOAD_SUCCESS = 
+            "Loaded new to-do list successfully!";
 
     public LoadHandler() {
         super(NAME, SYNTAX, FLAGS, OPTIONS, RESERVED);
@@ -17,5 +21,7 @@ public class LoadHandler extends CommandHandler {
     @Override
     public void execute(Logic logic, Command command) {
         logic.showChangeDataFilePathDialog();
+        logic.feedback(new FeedbackMessage(LOAD_SUCCESS, 
+                FeedbackType.INFO));
     }
 }
